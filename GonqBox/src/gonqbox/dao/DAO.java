d3050f52d52ae781ee10e8997f9ddd1974b21454
Gabriel Hounsome <imbeebo@gmail.com>
2016-09-22 09:26:38 -0400

    #6 added user model and added user login for DAO

--- GonqBox/src/gonqbox/dao/DAO.java
+++ GonqBox/src/gonqbox/dao/DAO.java
@@ -1,5 +1,66 @@
 package gonqbox.dao;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import gonqbox.models.*;
+
+public class DAO {
+	private static Connection conn = null;
+
+	private DAO() {
+	}
+
+	public static Connection getDAO() {
+		if (conn == null) {
+			try {
+				String url = "jdbc:mysql://localhost:3306/";
+				String dbName = "form";
+				String driver = "com.mysql.jdbc.Driver";
+				String userName = "root";
+				String password = "admin";
+				Class.forName(driver).newInstance();
+				conn = DriverManager.getConnection(url + dbName, userName, password);
+			} catch (SQLException e) {
+				System.out.println("SQL Exception: " + e);
+				return null;
+			} catch (InstantiationException e) {
+				System.out.println("Could not instansiate: " + e);
+				return null;
+			} catch (IllegalAccessException e) {
+				System.out.println("Cannot access: " + e);
+				return null;
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class not found: " + e);
+				return null;
+			}
+		}
+		return conn;
+	}
+
+	/**
+	 * Calls stored procedure to check
+	 * @return User object
+	 */
+	public static User loginUser(String username, String password) {
+		try {
+			PreparedStatement statement = null;
+			ResultSet rs = null;
+			String query = "SELECT first FROM tblUsers WHERE";
+
+			statement = conn.prepareStatement(query);
+			rs = statement.executeQuery();
+			User currentUser = null;
+			while (rs.next()) {
+				currentUser = new User(rs);
+			}
+			return currentUser;
+		} catch (SQLException e) {
+			System.out.println("Problem with the SQL: " + e);
+			return null;
+		}
+	}
 }
---
+++ GonqBox/src/gonqbox/models/User.java
@@ -0,0 +1,44 @@
+package gonqbox.models;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User {
+	private int user_id;
+	private String username;
+	private Date account_creation_date;
+	private String user_email;
+
+	public User(ResultSet rs) throws SQLException {
+		processRow(rs);
+	}
+	
+	public void processRow(ResultSet rs) throws SQLException {
+			user_id = rs.getInt("user_id");
+			username = rs.getString("username");
+			account_creation_date = rs.getDate("account_creation_date");
+			user_email = rs.getString("user_email");
+	}
+	
+	public int getUserID() {
+		return user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public Date getCreationDate() {
+		return account_creation_date;
+	}
+	
+	public String getEmail() {
+		return user_email;
+	}
+	
+	public void setEmail(String email) {
+		user_email = email;
+	}
+
+}
